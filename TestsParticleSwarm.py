8af9b8640a5a801c480b315326859c41beff4566
Josue Castillo <josue.daniel.cc@outlook.com>
2013-10-03 02:48:19 -0500

    Primer commit

---
+++ TestsParticleSwarm.py
@@ -0,0 +1,135 @@
+#-*- coding: utf-8 -*-
+from Utilerias import *
+from ParticleSwarm import *
+import unittest
+
+class TestUtilerias(unittest.TestCase):
+
+	def setUp(self):
+		self.ueas = cargar_datos()
+       
+	def testFoo(self):
+		self.assertTrue(True)
+
+	def test_Uea(self):
+		self.uea = UEA(1, "Introducción a la Física", 4, [], 0)
+		self.assertEqual(self.uea.clave, 1)
+		self.assertEqual(self.uea.nombre, "Introducción a la Física")
+		self.assertEqual(self.uea.creditos, 4)
+		self.assertEqual(self.uea.ueas_requeridas, [])
+		self.assertEqual(self.uea.creditos_requeridos, 0)
+
+	def test_cargar_datos(self):
+		self.assertGreater(len(self.ueas), 0)
+		creditos_totales = 0
+		for x in self.ueas.values():
+			creditos_totales += x.creditos
+		for x in self.ueas.values():
+			self.assertIsInstance(x, UEA)
+			self.assertTrue(x.clave <= len(self.ueas))
+			self.assertGreater(x.clave, 0)
+			self.assertGreater(x.creditos, 0)
+			self.assertGreaterEqual(x.creditos_requeridos, 0)
+			self.assertIsInstance(x.clave, int)
+			self.assertIsInstance(x.nombre, basestring)
+			self.assertIsInstance(x.creditos, int)
+			self.assertIsInstance(x.ueas_requeridas, list)
+			self.assertIsInstance(x.creditos_requeridos, int)
+			self.assertLess(x.creditos_requeridos, creditos_totales)
+			self.assertEqual(x.clave, self.ueas[x.clave].clave)
+			self.assertGreaterEqual(len(x.ueas_requeridas), 0)
+
+	def test_seriacion(self):
+		for x in self.ueas.values():
+			for clave_uea_requerida in x.ueas_requeridas:
+				self.assertGreater(x.clave, clave_uea_requerida)
+		self.assertIn(self.ueas[1].clave, self.ueas[7].ueas_requeridas)
+		self.assertIn(self.ueas[4].clave, self.ueas[7].ueas_requeridas)
+		self.assertIn(self.ueas[7].clave, self.ueas[8].ueas_requeridas)
+		self.assertNotIn(self.ueas[4].clave, self.ueas[8].ueas_requeridas)
+		self.assertIn(self.ueas[11].clave, self.ueas[17].ueas_requeridas)
+		self.assertIn(self.ueas[14].clave, self.ueas[17].ueas_requeridas)
+		#TODO Agregar más casos de prueba en la seriación
+
+
+	def test_seriacion_extendida(self):
+		extender_seriacion(self.ueas)
+		for x in self.ueas.values():
+			for clave_uea_requerida in x.ueas_requeridas:
+				self.assertGreater(x.clave, clave_uea_requerida)
+		self.assertIn(self.ueas[1].clave, self.ueas[7].ueas_requeridas)
+		self.assertIn(self.ueas[4].clave, self.ueas[7].ueas_requeridas)
+		self.assertIn(self.ueas[7].clave, self.ueas[8].ueas_requeridas)
+		self.assertIn(self.ueas[4].clave, self.ueas[8].ueas_requeridas)
+		self.assertIn(self.ueas[1].clave, self.ueas[8].ueas_requeridas)
+		#TODO Agregar más casos de prueba en la seriación
+
+	def test_calcular_limites_inferiores(self):
+		extender_seriacion(self.ueas)
+		limites_inferiores = calcular_limites_inferiores(self.ueas)
+		for limite_inferior in limites_inferiores.values():
+			self.assertLessEqual(limite_inferior, 11)
+			self.assertGreater(limite_inferior, 0)
+		for x in self.ueas.values():
+			if x.ueas_requeridas != []:
+				self.assertGreater(limites_inferiores[x.clave], 1)
+		self.assertEqual(limites_inferiores[1], 1)
+		self.assertEqual(limites_inferiores[4], 1)
+		self.assertEqual(limites_inferiores[1], 1)
+		self.assertEqual(limites_inferiores[7], 2)
+		self.assertEqual(limites_inferiores[8], 3)
+		self.assertEqual(limites_inferiores[10], 3)
+		self.assertEqual(limites_inferiores[11], 2)
+		self.assertEqual(limites_inferiores[14], 3)
+		self.assertEqual(limites_inferiores[62], 1)
+		#TODO Agregar mas casos de prueba
+
+	def test_Particula(self):
+		particula = Particula(len(self.ueas))
+		# particula.posicion = posicion = [1, 1, 2, 1, 7, 1, 2, 4, 2, 8, 2,
+		# 								2, 3, 3, 11, 3, 4, 4, 4, 8, 8, 5,
+		# 								5, 6, 5, 7, 6, 8, 5, 6, 5, 5, 9,
+		# 								7, 7, 6, 6, 11, 10, 8, 8, 7, 7, 6,
+		# 								9, 11, 9, 9, 10, 9, 2, 9, 8, 10,
+		# 								10, 1, 1, 4, 8, 9, 10 , 10, 11, 11,
+		# 								7, 11]
+		self.assertEqual(len(particula.posicion), len(self.ueas))
+		self.assertEqual(len(particula.velocidad), len(self.ueas))
+		for x in particula.posicion:
+			self.assertTrue(1 <= x <= 18)
+		for x in particula.velocidad:
+			self.assertTrue(-3 <= x <= 3)
+
+	def test_Particula_calcular_creditos_por_trimestre(self):
+		particula = Particula(len(self.ueas))
+		particula.calcular_creditos_por_trimestre(self.ueas)
+		creditos_totales = 0
+		for x in self.ueas.values():
+			creditos_totales += x.creditos
+		creditos_totales_por_trimestres = 0
+		for x in particula.trimestres.values():
+			self.assertGreaterEqual(x, 0)
+			self.assertLessEqual(x, creditos_totales)
+		for y in particula.trimestres:
+			creditos_totales_por_trimestres += particula.trimestres[y]
+		self.assertEqual(creditos_totales, creditos_totales_por_trimestres)
+		#Revisar la integridad de las llaves del diccionario
+		for x in xrange(len(particula.trimestres)):
+			self.assertGreaterEqual(x, 0)
+		
+
+	def test_Particula_calcular_calidad_EC(self):
+		particula = Particula(len(self.ueas))
+		particula.calcular_creditos_por_trimestre(self.ueas)
+	 	particula.calcular_calidad_EC()
+	 	self.assertGreaterEqual(particula.calidad_EC, 0)
+	 	self.assertEqual(particula.calidad_EC, particula.calidad)
+
+
+
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+    main()
---
+++ Utilerias.py
@@ -0,0 +1,118 @@
+#-*- coding: utf-8 -*-
+from random import randint
+import sys
+class UEA(object):
+    
+    def __init__(self, clave, nombre, creditos, ueas_requeridas, creditos_requeridos):
+        self.clave = clave
+        self.nombre = nombre
+        self.creditos = creditos
+        self.ueas_requeridas = ueas_requeridas
+        self.creditos_requeridos = creditos_requeridos
+
+class Particula(object):
+    __slots__ = ('posicion', 'velocidad', 'mejor_posicion', 'calidad_mejor_posicion', 'calidad',
+        'calidad_EC', 'calidad_RC', 'calidad_S', 'calidad_NT', 'ueas_violadas', 'trimestres')
+    def __init__(self, dimension):
+        self.posicion = [randint(1, 18) for x in xrange(dimension)]
+        self.velocidad = [randint(-3, 3) for x in xrange(dimension)]
+        self.mejor_posicion = self.posicion[:]
+        self.calidad_mejor_posicion = sys.maxint
+        self.calidad = 0
+        self.calidad_EC = 0
+        self.calidad_RC = 0
+        self.calidad_S = 0
+        self.calidad_NT = 0
+        self.ueas_violadas = []
+        self.trimestres = {}
+
+    def calcular_creditos_por_trimestre(self, ueas):
+        for i in xrange(1, 19):
+            self.trimestres[i] = 0
+        for x in xrange(len(self.posicion)):
+            uea_actual = ueas[x+1]
+            self.trimestres[self.posicion[x]] = self.trimestres[self.posicion[x]] + uea_actual.creditos
+
+    def calcular_calidad_EC(self):
+        self.calidad_EC = 0
+        self.calidad = 0
+        if self.trimestres[1] == 0:
+            self.calidad = self.calidad + 46
+            self.calidad_EC = self.calidad_EC + 46
+        else:
+            if self.trimestres[1] > 46:
+                diferencia = self.trimestres[1] - 46
+                self.calidad = self.calidad + diferencia
+                self.calidad_EC = self.calidad_EC + diferencia
+            # if trimestres[1] < 21 and trimestres[1] != 0:
+            #     diferencia = 21 - trimestres[1]
+            #     self.calidad = self.calidad + diferencia
+            #     self.calidad_EC = self.calidad_EC + diferencia
+        for h in xrange(2, len(self.trimestres)+1):
+            if self.trimestres[h] > 60:
+                diferencia = self.trimestres[h] - 60
+                self.calidad = self.calidad + diferencia
+                self.calidad_EC = self.calidad_EC + diferencia
+            # if trimestres[h] < 21 and trimestres[h] != 0:
+            #     diferencia = 21 - trimestres[h]
+            #     self.calidad = self.calidad + diferencia
+            #     self.calidad_EC = self.calidad_EC + diferencia
+
+        for h in xrange(2,len(self.trimestres)):
+            if self.trimestres[h] == 0:
+                if self.trimestres[h+1] > 0:
+                    self.calidad = self.calidad + 60
+                    self.calidad_EC = self.calidad_EC + 60
+      
+
+def extender_seriacion(ueas):
+    for uea in ueas.values():
+        revisar_seriacion(uea ,uea.ueas_requeridas , ueas)
+
+def revisar_seriacion(uea, ueas_requeridas,ueas):
+    if ueas_requeridas == []:
+        return
+    else:
+        for x in ueas_requeridas:
+            if x not in uea.ueas_requeridas:
+                uea.ueas_requeridas.append(x)
+            revisar_seriacion(uea, ueas[x].ueas_requeridas, ueas)
+
+def cargar_datos():
+    ueas = {}
+    archivo = open("Seriacion_UEA_Creditos.txt", "r")
+    datos = archivo.readlines()
+    for registro in datos:
+        linea = registro.split('-')
+        clave = int(linea[0])
+        nombre = linea[1]
+        creditos = int(linea[2])
+        ueas_requeridas = []
+        for x in linea[3].split(';'):
+            if int(x) != 0:
+                ueas_requeridas.append(int(x))
+        creditos_requeridos = int(linea[4])
+        
+        uea = UEA(clave, nombre, creditos, ueas_requeridas, creditos_requeridos)
+        ueas[uea.clave] = uea
+    return ueas
+
+def calcular_limites_inferiores(ueas):
+    limites_inferiores = {}
+    for uea in ueas.values():
+        if uea.ueas_requeridas == []:
+            limites_inferiores[uea.clave] = 1
+        else:
+            limites_inferiores[uea.clave] = 2
+    for n in xrange(2,19):
+        calcular_limites(n, limites_inferiores, ueas)
+    return limites_inferiores
+
+def calcular_limites(n,limites_inferiores, ueas):
+    for uea_actual in ueas.values():
+        if uea_actual.ueas_requeridas !=[]:
+            for clave_uea_seriada in uea_actual.ueas_requeridas:
+                if uea_actual.creditos_requeridos < ueas[clave_uea_seriada].creditos_requeridos:
+                    uea_actual.creditos_requeridos = ueas[clave_uea_seriada].creditos_requeridos
+                if limites_inferiores[clave_uea_seriada] > n-1:
+                    limites_inferiores[uea_actual.clave] = n+1
